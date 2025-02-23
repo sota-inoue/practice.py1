4bee83171258364797dd97cf67670ca2d80a0871
inosou <kiribachi.12@gmail.com>
2025-02-23 21:23:20 +0900

    paizaラーニングのC103ロボット芸人という問題を解きました。

---
+++ C103ロボット芸人/roboGeinin.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+N,M = map(int, input().split());
+move = [input().split() for i in range(M)]
+
+for i in range(N):
+    judge = True;
+    output = [];
+    for j in move:
+        if (i + 1) % int(j[0]) == 0:
+            output.append(j[1]);
+            judge = False;
+    if judge == True:
+        print(i + 1);
+    else:
+        print(*output);t);
